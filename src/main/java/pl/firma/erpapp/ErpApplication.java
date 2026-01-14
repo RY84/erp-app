13895d448006c377083a260022208ae32a15f82f
Łukasz Maliszewski <pazuzury84@gmail.com>
2026-01-14 14:03:48 +0100

    test #3

--- src/main/java/pl/firma/erpapp/ErpApplication.java
+++ src/main/java/pl/firma/erpapp/ErpApplication.java
@@ -2,6 +2,9 @@ package pl.firma.erpapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ErpApplication {
@@ -10,4 +13,15 @@ public class ErpApplication {
 		SpringApplication.run(ErpApplication.class, args);
 	}
 
+	@Configuration
+	static class StaticConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry
+					.addResourceHandler("/hls/**")
+					.addResourceLocations("file:/home/lmaliszewski/hls/")
+					.setCachePeriod(0);
+		}
+	}
 }
